c571870cc14dcaa6eb90412408e71bf53a7bc6d0
Ben Koziol <bkoziol@redlineperf.com>
2025-10-10 16:08:45 -0600

    minor [ci skip]

--- src/test/test_mm_eval/test_driver/test_package.py
+++ src/test/test_mm_eval/test_driver/test_package.py
@@ -117,6 +117,8 @@ def test_all_packages(mm_eval_runner_test_data: MMEvalRunnerTestData, mocker: Mo
 
     package.run(TaskKey.SAVE_PAIRED)
 
+    assert package.mm_package_output_dir.exists()
+
     m_analysis.read_control.assert_called_once()
     m_analysis.open_models.assert_called_once()
     m_analysis.open_obs.assert_called_once()
